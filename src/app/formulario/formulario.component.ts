d7647ec3250cc1c5cc0ffc6e5a2e11d5db1bd56a
Daniel <DANDANUS1999@gmail.com>
2020-07-22 15:07:23 -0300

    listado-personas-commit

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Persona } from '../persona.model';
+import { LoggingService } from '../LoggingService.service';
+import { PersonasService } from '../personas.service';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css'],
+})
+export class FormularioComponent implements OnInit {
+
+  nombreInput: string;
+  apellidoInput: string;
+
+
+
+  constructor(private loggingService: LoggingService,
+              private personasService: PersonasService) {
+                this.personasService.saludar.subscribe(
+                  (indice: number) => alert("El indice es: " + indice)
+                );
+               }
+
+  ngOnInit(): void {
+  }
+
+  onAgregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personasService.agregarPersona(persona1);
+  }
+}
